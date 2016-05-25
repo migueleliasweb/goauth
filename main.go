558250c20e7d0a54c20c42c00b4ad6e4833ffeb9
Miguel E dos Santos <miguel.santos@sieve.com.br>
2016-05-25 19:21:02 -0300

    GO !

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -10,25 +11,51 @@ import (
 )
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	w.Write([]byte(r.URL.Path + "FOO"))
+
+	//w.Write([]byte(r.URL.Path + "FOO"))
+
+	//enforce POST
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
+	}
+
+	bodyBytes, bodyError := ioutil.ReadAll(r.Body)
+
+	if bodyError != nil {
+		log.Fatalln(bodyError)
+	}
+
+	w.Write(bodyBytes)
+
 }
 
-func main() {
-	RedisSchemeURL := flag.String("redis-url", "redis://@localhost:6379/0", "Url to connect to redis")
-	HTTPAddress := flag.String("http-address", ":8080", "Http address to bind to")
+func setupRedis(RedisSchemeURL *string) (connection redis.Conn) {
+	connection, error := redis.DialURL(*RedisSchemeURL)
+	if error != nil {
+		log.Fatalln("Could not connect to REDIS:", error)
+	} else {
+		fmt.Println("Connected to REDIS:", *RedisSchemeURL)
+	}
+
+	return connection
+}
+
+func setupFlags() (RedisSchemeURL *string, HTTPAddress *string) {
+	RedisSchemeURL = flag.String("redis-url", "redis://@localhost:6379/0", "Url to connect to redis")
+	HTTPAddress = flag.String("http-address", ":8080", "Http address to bind to")
 
 	flag.Parse()
 
-	c, err := redis.DialURL(*RedisSchemeURL)
-	if err != nil {
-		log.Fatalln("Could not connect to REDIS: ", err)
-	} else {
-		fmt.Println("Connected to REDIS: ", *RedisSchemeURL)
-	}
+	return RedisSchemeURL, HTTPAddress
+}
+
+func main() {
+	RedisSchemeURL, HTTPAddress := setupFlags()
+	redisConnection := setupRedis(RedisSchemeURL)
 
 	http.HandleFunc("/auth", authHandler)
 	log.Fatalln(http.ListenAndServe(*HTTPAddress, nil))
 
-	defer c.Close()
+	defer redisConnection.Close()
 }
